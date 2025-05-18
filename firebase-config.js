a899285dfc5f72e0893c86beb04522da553d3ca0
YwwA <yawwa1212@gmail.com>
2025-05-19 08:49:55 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyAWPJpsr_zqn6VUM2XPeC7uHgdqgs_maJ4",
   authDomain: "fuc333-928d9.firebaseapp.com",
   projectId: "fuc333-928d9",
-  storageBucket: "fuc333-928d9.firebasestorage.app",
+  storageBucket: "  storageBucket: "fuc333-928d9.appspot.com",
+",
   messagingSenderId: "337756181444",
   appId: "1:337756181444:web:6f4846f5dc09808ed30a91",
   measurementId: "G-YZQJG4V6WL"
