5b49801715a71052be0566d526d291a372d4edf1
YwwA <yawwa1212@gmail.com>
2025-05-19 08:42:12 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdefg"
+};
+
+firebase.initializeApp(firebaseConfig);
